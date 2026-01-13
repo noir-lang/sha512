4af5d4301fde5717f22c0fa73462b046b1956c5d
github-action-benchmark <github@users.noreply.github.com>
2026-01-13 12:18:36 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for 2ec1d1d99f31325f00f07a42578b7c82483cd3cc

---
+++ dev/bench/data.js
@@ -0,0 +1,107 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768306716929,
+  "repoUrl": "https://github.com/noir-lang/sha512",
+  "entries": {
+    "ACIR Opcodes": [
+      {
+        "commit": {
+          "author": {
+            "email": "26174818+jialinli98@users.noreply.github.com",
+            "name": "Jialin Li",
+            "username": "jialinli98"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2ec1d1d99f31325f00f07a42578b7c82483cd3cc",
+          "message": "chore!: remove wrapping_add + update oracle testing + update benchmarks (#13)",
+          "timestamp": "2026-01-13T12:17:35Z",
+          "tree_id": "bb3c4cde5c0c5c3f1e5851737f14e4d4b8a631ca",
+          "url": "https://github.com/noir-lang/sha512/commit/2ec1d1d99f31325f00f07a42578b7c82483cd3cc"
+        },
+        "date": 1768306716138,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_sha512_1_block.json/main",
+            "value": 13216,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "bench_sha512_2_blocks.json/main",
+            "value": 26101,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "bench_sha512_3_blocks.json/main",
+            "value": 38986,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "bench_sha512_4_blocks.json/main",
+            "value": 51871,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "bench_sha512_compression.json/main",
+            "value": 13392,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "bench_sha512_var_1_block.json/main",
+            "value": 14107,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "bench_sha512_var_2_blocks.json/main",
+            "value": 27803,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "bench_sha512_var_3_blocks.json/main",
+            "value": 41484,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "bench_sha512_var_4_blocks.json/main",
+            "value": 55181,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha384_1.json/main",
+            "value": 13981,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha384_100.json/main",
+            "value": 14080,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha384_256.json/main",
+            "value": 41357,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha512_1.json/main",
+            "value": 13997,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha512_100.json/main",
+            "value": 14096,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha512_256.json/main",
+            "value": 41373,
+            "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ]
+  }
+}
